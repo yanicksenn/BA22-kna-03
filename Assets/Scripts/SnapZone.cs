4628632eb2524500a8ab7b595e11f8f5de1db33a
Yanick Senn <sennyan2@students.zhaw.ch>
2022-04-07 15:25:53 +0200

    Remove DefaultNamespace

--- Assets/Scripts/SnapZone.cs
+++ Assets/Scripts/SnapZone.cs
@@ -1,91 +1,87 @@
-using System;
 using UnityEngine;
 
-namespace DefaultNamespace
+[RequireComponent(typeof(Collider))]
+public class SnapZone : MonoBehaviour
 {
-    [RequireComponent(typeof(Collider))]
-    public class SnapZone : MonoBehaviour
+    [SerializeField] 
+    private Transform snapReference;
+    public Transform SnapReference
     {
-        [SerializeField] 
-        private Transform snapReference;
-        public Transform SnapReference
-        {
-            get => snapReference;
-            set => snapReference = value;
-        }
+        get => snapReference;
+        set => snapReference = value;
+    }
 
 
         private Snappable _snappedObject;
         private GameObject _preview;
 
-        private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerStay(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var snappable = other.GetComponent<Snappable>();
+        if (snappable == null)
+            return;
+
+        if (_snappedObject != null)
+            return;
+
+        if (snappable.IsGrabbed)
         {
-            OnTriggerStay(other);
+            ShowPreview(snappable);
+            return;
         }
 
-        private void OnTriggerStay(Collider other)
-        {
-            var snappable = other.GetComponent<Snappable>();
-            if (snappable == null)
-                return;
+        HidePreview();
 
-            if (_snappedObject != null)
-                return;
+        _snappedObject = snappable;
+        _snappedObject.Grabbable.OnGrab.AddListener(OnGrab);
+        _snappedObject.Snap();
+        
+        var snappedtransform = _snappedObject.transform;
+        snappedtransform.position = snapReference.position;
+        snappedtransform.rotation = snapReference.rotation;
+    }
 
-            if (snappable.IsGrabbed)
-            {
-                ShowPreview(snappable);
-                return;
-            }
+    private void OnTriggerExit(Collider other)
+    {
+        var snappable = other.GetComponent<Snappable>();
+        if (snappable == null)
+            return;
 
-            HidePreview();
+        if (!snappable.IsGrabbed)
+            return;
+        
+        HidePreview();
+    }
 
-            _snappedObject = snappable;
-            _snappedObject.Grabbable.OnGrab.AddListener(OnGrab);
-            _snappedObject.Snap();
-            
-            var snappedtransform = _snappedObject.transform;
-            snappedtransform.position = snapReference.position;
-            snappedtransform.rotation = snapReference.rotation;
-        }
+    private void OnGrab()
+    {
+        _snappedObject.Grabbable.OnGrab.RemoveListener(OnGrab);
+        _snappedObject = null;
+    }
 
-        private void OnTriggerExit(Collider other)
-        {
-            var snappable = other.GetComponent<Snappable>();
-            if (snappable == null)
-                return;
+    private void ShowPreview(Snappable snappable)
+    {
+        if (_preview != null)
+            return;
 
-            if (!snappable.IsGrabbed)
-                return;
-            
-            HidePreview();
-        }
+        var preview = snappable.Preview;
+        if (preview == null) 
+            return;
+        
+        _preview = Instantiate(preview, snapReference.position, snapReference.rotation);
+    }
 
-        private void OnGrab()
-        {
-            _snappedObject.Grabbable.OnGrab.RemoveListener(OnGrab);
-            _snappedObject = null;
-        }
-
-        private void ShowPreview(Snappable snappable)
-        {
-            if (_preview != null)
-                return;
-
-            var preview = snappable.Preview;
-            if (preview == null) 
-                return;
-            
-            _preview = Instantiate(preview, snapReference.position, snapReference.rotation);
-        }
-
-        private void HidePreview()
-        {
-            if (_preview == null)
-                return;
-            
-            Destroy(_preview);
-            _preview = null;
-        }
+    private void HidePreview()
+    {
+        if (_preview == null)
+            return;
+        
+        Destroy(_preview);
+        _preview = null;
     }
 }
--- Assets/Scripts/Snappable.cs
+++ Assets/Scripts/Snappable.cs
@@ -1,29 +1,26 @@
 using UnityEngine;
 
-namespace DefaultNamespace
+[RequireComponent(typeof(OVRGrabbableExtension))]
+[RequireComponent(typeof(Rigidbody))]
+public class Snappable : MonoBehaviour
 {
-    [RequireComponent(typeof(OVRGrabbableExtension))]
-    [RequireComponent(typeof(Rigidbody))]
-    public class Snappable : MonoBehaviour
-    {
-        [SerializeField]
-        private GameObject preview;
-        public GameObject Preview => preview;
+    [SerializeField]
+    private GameObject preview;
+    public GameObject Preview => preview;
 
-        private OVRGrabbableExtension _grabbable;
-        public OVRGrabbableExtension Grabbable => _grabbable;
-        private Rigidbody _rigidbody;
+    private OVRGrabbableExtension _grabbable;
+    public OVRGrabbableExtension Grabbable => _grabbable;
+    private Rigidbody _rigidbody;
 
     public bool IsGrabbed => _grabbable.isGrabbed;
 
-        private void Awake()
-        {
-            _grabbable = GetComponent<OVRGrabbableExtension>();
-            _rigidbody = GetComponent<Rigidbody>();
-        }
-        public void Snap()
-        {
-            _rigidbody.isKinematic = true;
-        }
+    private void Awake()
+    {
+        _grabbable = GetComponent<OVRGrabbableExtension>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    public void Snap()
+    {
+        _rigidbody.isKinematic = true;
     }
 }
