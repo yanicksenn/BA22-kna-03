9d078a50a33c72afcb74bd1034b51528437050ab
WEBER Rhiana <rhiana.weber@danone.com>
2022-04-25 14:15:26 +0200

    added blackboard with exercise

---
+++ Assets/Scripts/Runtime/Exercise/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Exercise",menuName = "BA22/Create exercise")]
+public class Exercise: ScriptableObject
+{
+    [SerializeField] private int exerciseNumber;
+    public int ExerciseNumber => exerciseNumber;
+    
+    [SerializeField] private string exerciseName;
+    public string ExerciseName => exerciseName;
+    
+    [SerializeField,TextArea] private string exerciseDescription;
+    public string ExerciseDescription => exerciseDescription;
+
+}
