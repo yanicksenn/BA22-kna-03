495f52461b15ff3244c62c247d3738218da26f67
WEBERRHI <weberrhi@students.zhaw.ch>
2022-04-30 22:42:04 +0200

    Add Tutorial Highlighter

---
+++ Assets/Scripts/Runtime/Tutorial/TutorialHighlighter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Runtime.Tutorial
+{
+    public class TutorialHighlighter : MonoBehaviour
+    {
+        [SerializeField]
+        private TutorialSlide tutorialSlide;
+        public TutorialSlide TutorialSlide
+        {
+            get => tutorialSlide;
+            set => tutorialSlide = value;
+        }
+
+        private void Awake()
+        {
+            tutorialSlide.SlideEvents.OnShowEvent.AddListener(Activate);
+            tutorialSlide.SlideEvents.OnHideEvent.AddListener(Deactivate);
+        }
+
+        private void OnDestroy()
+        {
+            tutorialSlide.SlideEvents.OnShowEvent.RemoveListener(Activate);
+            tutorialSlide.SlideEvents.OnHideEvent.RemoveListener(Deactivate);
+        }
+
+        private void Activate()
+        {
+            tutorialSlide.TutorialEvent.OnInvocation += Deactivate;
+            gameObject.SetActive(true);
+        }
+
+        private void Deactivate()
+        {
+            tutorialSlide.TutorialEvent.OnInvocation -= Deactivate;
+            gameObject.SetActive(false);
+        }
+    }
+}
