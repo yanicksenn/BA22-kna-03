8e34db30b09ecad370fe288c13bda70276f9790c
WEBERRHI <weberrhi@students.zhaw.ch>
2022-04-30 18:13:42 +0200

    Removed Exercisemanager from Board and created empty gameobject to hold the script

---
+++ Assets/Scripts/Runtime/Tutorial/TutorialManager.cs
@@ -0,0 +1,18 @@
+﻿using Runtime.Presentation;
+using UnityEngine;
+
+namespace Runtime.Tutorial
+{
+    public class TutorialManager : MonoBehaviour, INavigationCondidition
+    {
+        public bool AllowsNext(AbstractSlide slide)
+        {
+            return false;
+        }
+
+        public bool AllowsPrevious(AbstractSlide slide)
+        {
+            return false;
+        }
+    }
+}
