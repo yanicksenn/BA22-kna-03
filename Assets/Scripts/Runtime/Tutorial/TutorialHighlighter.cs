5567ecfdf249733840e3873c4f7ac759b6a66b72
Yanick Senn <sennyan2@students.zhaw.ch>
2022-05-01 12:14:55 +0200

    Add tutorial highlighter example

--- Assets/Scripts/Runtime/Tutorial/TutorialHighlighter.cs
+++ Assets/Scripts/Runtime/Tutorial/TutorialHighlighter.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using Runtime.Presentation;
+using UnityEngine;
 
 namespace Runtime.Tutorial
 {
     public class TutorialHighlighter : MonoBehaviour
     {
+        [SerializeField] 
+        private Presenter presenter;
+        public Presenter Presenter
+        {
+            get => presenter;
+            set => presenter = value;
+        }
+
         [SerializeField]
         private TutorialSlide tutorialSlide;
         public TutorialSlide TutorialSlide
@@ -12,10 +21,21 @@ namespace Runtime.Tutorial
             set => tutorialSlide = value;
         }
 
+        [SerializeField] 
+        private bool prohibitManualNext;
+        public bool ProhibitManualNext
+        {
+            get => prohibitManualNext;
+            set => prohibitManualNext = value;
+        }
+
         private void Awake()
         {
             tutorialSlide.SlideEvents.OnShowEvent.AddListener(Activate);
             tutorialSlide.SlideEvents.OnHideEvent.AddListener(Deactivate);
+            
+            // Presenter shows first slide on Start()
+            Deactivate();
         }
 
         private void OnDestroy()
@@ -26,14 +46,31 @@ namespace Runtime.Tutorial
 
         private void Activate()
         {
-            tutorialSlide.TutorialEvent.OnInvocation += Deactivate;
+            tutorialSlide.TutorialEvent.OnInvocation += TutorialEventInvoked;
             gameObject.SetActive(true);
+        }
+
+        private void TutorialEventInvoked()
+        {
+            Deactivate();
+            ShowNextSlideOnPresenterIfPossible();
         }
 
         private void Deactivate()
         {
-            tutorialSlide.TutorialEvent.OnInvocation -= Deactivate;
+            tutorialSlide.TutorialEvent.OnInvocation -= TutorialEventInvoked;
             gameObject.SetActive(false);
+        }
+
+        private void ShowNextSlideOnPresenterIfPossible()
+        {
+            if (presenter == null)
+                return;
+            
+            if (prohibitManualNext)
+                return;
+            
+            presenter.Next();
         }
     }
 }
