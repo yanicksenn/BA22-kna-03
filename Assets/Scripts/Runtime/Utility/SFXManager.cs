53a7acbe1384d7570b87e8874e2c374345e1ffa4
WEBERRHI <weberrhi@students.zhaw.ch>
2022-05-02 17:05:29 +0200

    added sounds to scene

---
+++ Assets/Scripts/Runtime/Utility/SFXManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Runtime.Utility
+{
+    public class SFXManager : MonoBehaviour
+    {
+        [SerializeField] private AudioSource snap;
+        [SerializeField] private AudioSource buttonClick;
+
+        public void PlaySnapSound()
+        {
+            snap.Play();
+        }
+
+        public void PlayButtonClickSound()
+        {
+            buttonClick.Play();
+        }
+    }
+}
