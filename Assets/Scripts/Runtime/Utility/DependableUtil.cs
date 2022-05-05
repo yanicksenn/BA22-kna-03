8d7c5284cbd40af04624bd807f37233991012b5e
WEBERRHI <weberrhi@students.zhaw.ch>
2022-05-05 11:42:04 +0200

    Fixed cyclic dependency

--- Assets/Scripts/Runtime/Utility/DependableUtil.cs
+++ Assets/Scripts/Runtime/Utility/DependableUtil.cs
@@ -2,23 +2,22 @@ using System.Collections.Generic;
 
 public static class DependableUtil
 {
-    public static bool HasCyclicDependencies(IDependable dependable)
+    public static bool HasCyclicDependencies(IDependable currentDependable)
     {
-        return HasCyclicDependencies(dependable, new HashSet<IDependable>());
+        return HasCyclicDependencies(currentDependable, currentDependable, true);
     }
     
-    private static bool HasCyclicDependencies(IDependable dependable, ISet<IDependable> cache)
+    private static bool HasCyclicDependencies(IDependable currentDependable, IDependable originalDependable, bool firstCall)
     {
-        if (cache.Contains(dependable))
+        if (!firstCall && currentDependable == originalDependable)
             return true;
-        
-        cache.Add(dependable);
-        var directDependencies = dependable.GetDependencies();
+
+        var directDependencies = currentDependable.GetDependencies();
         if (directDependencies == null)
             return false;
 
-        foreach (var dependency in directDependencies)
-            if (HasCyclicDependencies(dependency, cache))
+        foreach (var nextDependency in directDependencies)
+            if (HasCyclicDependencies(nextDependency, originalDependable, false))
                 return true;
 
         return false;
